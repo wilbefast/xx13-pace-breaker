e24b107fb1f64e7ebc76b5f449824630b59372d8
Kevin "Gaeel" Bradshaw-Rodriguez <k@xi.gd>
2013-01-25 19:10:07 +0100

    Serveur HTML UP!

---
+++ src/server.js
@@ -0,0 +1,44 @@
+mime = require('mime')
+  , app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs');
+
+/**/
+repl = require('repl');
+rep = repl.start({
+  prompt: "server> ",
+  input: process.stdin,
+  output: process.stdout,
+  useGlobal: true,
+  ignoreUndefined: true
+});
+
+rep.on('exit', function () {
+  console.log('Got "exit" event from repl!');
+  process.exit();
+});
+/**/
+
+io.set('log level',1)
+
+app.listen(1986);
+
+function handler (req, res) {
+  var filename = req.url;
+  if (filename === '/') {
+    filename = '/index.html';
+  }
+  fs.readFile(__dirname + filename,
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Mistakes were made...');
+    }
+    res.writeHead(200, {'Content-Type': mime.lookup(filename)});
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+	socket.emit("hello");
+});
