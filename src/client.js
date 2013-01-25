a5cf8dcd1e5333ce577e46d6a9d2c6b259469b79
Kevin "Gaeel" Bradshaw-Rodriguez <k@xi.gd>
2013-01-25 20:36:51 +0100

    Structure de base

---
+++ src/client.js
@@ -0,0 +1,8 @@
+var socket = io.connect(location.origin);
+socket.on('load',function(data){
+  if (data.callback)
+    loadScript(data.url, data.callback);
+  else
+    loadScript(data.url);
+});
+socket.on('hello',function() {console.log("Dude...");})
